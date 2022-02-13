0b3f8c220d03ab55e91f331ae3efac2e1d916c35
Natasha <njohnson533@gmail.com>
2022-02-13 18:02:33 -0500

    Updates

---
+++ precache-manifest.e9b754f64743340a3dc0066080732380.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1bf353bb1601d326fdbad79c2ff911",
+    "url": "/memorygame/index.html"
+  },
+  {
+    "revision": "09a2b191ca434dd45c51",
+    "url": "/memorygame/static/js/2.15a18f7c.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/memorygame/static/js/2.15a18f7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e00584cde1f10b75ac6",
+    "url": "/memorygame/static/js/main.705940bb.chunk.js"
+  },
+  {
+    "revision": "5da83b48386092e8ab4c",
+    "url": "/memorygame/static/js/runtime-main.50063c8d.js"
+  },
+  {
+    "revision": "5e56f6df12998117589064a39aaf4d39",
+    "url": "/memorygame/static/media/0.5e56f6df.jpg"
+  },
+  {
+    "revision": "66e813ce0fb9781d87bebcb824729be5",
+    "url": "/memorygame/static/media/1.66e813ce.jpg"
+  },
+  {
+    "revision": "9a29c431003abaef2581c228f7b16a44",
+    "url": "/memorygame/static/media/2.9a29c431.jpg"
+  },
+  {
+    "revision": "9f6cdb8a58a58e5a80c59fa69052c508",
+    "url": "/memorygame/static/media/3.9f6cdb8a.jpg"
+  },
+  {
+    "revision": "d9127c30eae6da0a2f5dfcc0c0a5dd37",
+    "url": "/memorygame/static/media/4.d9127c30.jpg"
+  },
+  {
+    "revision": "11cd3c781376d16ca6e15a023e2f7d5a",
+    "url": "/memorygame/static/media/5.11cd3c78.jpg"
+  },
+  {
+    "revision": "0a5217a1b3067c968783edbb4d2b5894",
+    "url": "/memorygame/static/media/6.0a5217a1.jpg"
+  },
+  {
+    "revision": "1e553912313038f36c5e01801df0b0c6",
+    "url": "/memorygame/static/media/7.1e553912.jpg"
+  },
+  {
+    "revision": "1f4efb8a2842a3615c628fc9c347134f",
+    "url": "/memorygame/static/media/8.1f4efb8a.jpg"
+  }
+]);
